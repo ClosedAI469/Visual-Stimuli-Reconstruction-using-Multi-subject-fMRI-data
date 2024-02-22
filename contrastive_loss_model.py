e415f3feaa2ce2b55587d0a09d7465c251880247
Sam Scholnick Hughes <samscholnickhughes@Sams-MacBook-Pro.local>
2024-02-21 21:09:14 -0700

    add functionality to display test loss between traiining epochs

--- contrastive_loss_model.py
+++ contrastive_loss_model.py
@@ -8,11 +8,12 @@ from torch.utils.data import Dataset
 import torch.nn.functional as F
 
 class Contrastive_loss_trainer():
-    def __init__(self, fMRI_encoder, dataSet):
+    def __init__(self, fMRI_encoder, trainSet, testSet):
         self.fMRI_encoder = fMRI_encoder
         self.image_encoder = AutoencoderKL.from_pretrained("CompVis/stable-diffusion-v1-4", subfolder="vae")
         self.image_encoder.eval()
-        self.dataSet = dataSet
+        self.trainSet = trainSet
+        self.testSet = testSet
 
     def encode_image(self, x):
         # return tensor of size 16384
@@ -29,11 +30,13 @@ class Contrastive_loss_trainer():
 
     def train(self, epochs=1, batchsize=16):
         self.fMRI_encoder.train()
-        train_loader = DataLoader(self.dataset, batch_size=batchsize, shuffle=True)
+        train_loader = DataLoader(self.trainSetset, batch_size=batchsize, shuffle=True)
+        test_loader = DataLoader(self.testSet, batch_size=batchsize)
         loss_fn = torch.nn.CrossEntropyLoss()
         optimizer = torch.optim.Adam(self.fMRI_encoder.parameters())
 
         for epoch in range(epochs):
+            self.fMRI_encoder.train()
             for i, data in enumerate(train_loader):
                 fMRIs, images = data
                 encoded_images = self.encode_image(images)
@@ -47,6 +50,24 @@ class Contrastive_loss_trainer():
                 loss.backwards()
 
                 optimizer.step()
+            self.fMRI_encoder.eval()
+            running_loss = 0
+            j = 0
+            for j, data in enumerate(test_loader):
+                fMRIs, images = data
+                encoded_images = self.encode_image(images)
+
+                fMRI_encodings = self.fMRI_encoder(fMRIs)
+                cos_sim_matrix = self.NT_Xent_loss(fMRI_encodings, encoded_images)
+
+                targets = torch.eye(batchsize)
+                loss = loss_fn(cos_sim_matrix, targets)
+                running_loss += loss
+
+            avg_loss = running_loss / j
+            print(f"test loss: {avg_loss}")
+
+            
 
 
 
